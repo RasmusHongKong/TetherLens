dccf805e9cb193fd77d3e153a68ad6f860bdc244
RasmusHongKong <120019919+RasmusHongKong@users.noreply.github.com>
2026-08-18 11:35:56 +0800

    Add secondary webpage source type

--- src/tetherlens_ingest/models.py
+++ src/tetherlens_ingest/models.py
@@ -11,6 +11,7 @@ class SourceType(StrEnum):
     MANUFACTURER_WEBPAGE = "manufacturer_webpage"
     MANUFACTURER_JSON = "manufacturer_json"
     MANUFACTURER_DOCUMENT = "manufacturer_document"
+    SECONDARY_WEBPAGE = "secondary_webpage"
 
 
 class ProductType(StrEnum):
