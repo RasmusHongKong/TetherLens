b0c8f0a4052ce7335301249e05d9d7079cb8ff29
RasmusHongKong <120019919+RasmusHongKong@users.noreply.github.com>
2026-08-25 11:08:46 +0800

    Use NLG connection-interface adapter layer

--- src/tetherlens_ingest/adapters/__init__.py
+++ src/tetherlens_ingest/adapters/__init__.py
@@ -1,7 +1,7 @@
 from .hilti_tool_attachment import HiltiAdapter
 from .klein import KleinAdapter
 from .milwaukee import MilwaukeeAdapter
-from .nlg_compat import NLGAdapter
+from .nlg_interfaces import NLGAdapter
 from .stopdrop import StopDropAdapter
 
 __all__ = [
