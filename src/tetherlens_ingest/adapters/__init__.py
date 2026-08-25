461df2ec0b4fa3111a4e9ab92e743172a177dcb1
RasmusHongKong <120019919+RasmusHongKong@users.noreply.github.com>
2026-08-25 09:07:06 +0800

    Expose Klein adapter

--- src/tetherlens_ingest/adapters/__init__.py
+++ src/tetherlens_ingest/adapters/__init__.py
@@ -1,6 +1,13 @@
 from .hilti_tool_attachment import HiltiAdapter
+from .klein import KleinAdapter
 from .milwaukee import MilwaukeeAdapter
 from .nlg_compat import NLGAdapter
 from .stopdrop import StopDropAdapter
 
-__all__ = ["NLGAdapter", "HiltiAdapter", "StopDropAdapter", "MilwaukeeAdapter"]
+__all__ = [
+    "NLGAdapter",
+    "HiltiAdapter",
+    "KleinAdapter",
+    "StopDropAdapter",
+    "MilwaukeeAdapter",
+]
