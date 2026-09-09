3992c7615890122caebfc28e1816691f4112c632
RasmusHongKong <120019919+RasmusHongKong@users.noreply.github.com>
2026-09-09 09:54:15 +0800

    Restore anchor-form adapter entry point

--- src/tetherlens_ingest/adapters/__init__.py
+++ src/tetherlens_ingest/adapters/__init__.py
@@ -1,7 +1,7 @@
 from .hilti_tool_attachment import HiltiAdapter
 from .klein import KleinAdapter
 from .milwaukee import MilwaukeeAdapter
-from .nlg_target_interface_form import NLGAdapter
+from .nlg_anchor_interface_form import NLGAdapter
 from .stopdrop import StopDropAdapter
 
 __all__ = [
